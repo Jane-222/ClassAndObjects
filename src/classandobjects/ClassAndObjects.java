2fe3f8c2a63bc2900421b2c9ae6f69f03e6a788e
2022221 <2022221@student.cct.ie>
2023-10-04 13:16:32 +0100

    Added an import statement for the new shapes package and Triangle class.

--- src/classandobjects/ClassAndObjects.java
+++ src/classandobjects/ClassAndObjects.java
@@ -4,6 +4,8 @@
  */
 package classandobjects;
 
+import Shapes.Triangle;
+
 /**
  *
  * @author zhiya
@@ -14,7 +16,34 @@ public class ClassAndObjects {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Triangle tri1 = new Triangle();
+        Triangle tri2 = new Triangle();
+        Triangle tri3 = new Triangle();
+        Triangle tri4 = new Triangle();
+        Triangle tri5 = new Triangle();
+        Triangle tri6 = new Triangle();
+        
+        int[] sideLengths = new int[18];
+        //The lengths of tri1's sides will be first, then the lengths of tri2's sides etc
+        sideLengths[0] = 5;
+        sideLengths[1] = 8;
+        sideLengths[2] = 9;
+        
+        //tri5 (4,7,2)
+        sideLengths[12] = 4;
+        sideLengths[13] = 7;
+        sideLengths[14] = 2;
+        
+        //we want to set the middle side of each triangle to be 3
+        sideLengths[1] = 3;
+        sideLengths[4] = 3;
+        sideLengths[7] = 3;
+        sideLengths[10] = 3;
+        sideLengths[13] = 3;
+        sideLengths[16] = 3;
+        
+        Person zhiya = new Person();
+        zhiya.favTriangle = tri5;
     }
     
 }
---
+++ src/classandobjects/Person.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classandobjects;
+
+import Shapes.Triangle;
+
+
+
+/**
+ *
+ * @author zhiya
+ */
+public class Person {
+    int age;
+    String name;
+    double height;
+    boolean isProgrammer;
+    Triangle favTriangle;//favourire triangle 
+}
